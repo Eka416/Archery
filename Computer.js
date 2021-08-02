21926e092f564edfdc58f793fdd7aabc5320ad2d
Eka416 <87167166+Eka416@users.noreply.github.com>
2021-08-02 11:05:04 -0700

    Add files via upload

---
+++ Computer.js
@@ -0,0 +1,24 @@
+class Computer {
+    constructor(x,y,w,h) {
+        var options = {
+            isStatic: true
+        }
+    
+    this.bob = Bodies.rectangle(x,y,w,h,options)
+    this.w = w
+    this.h = h
+    this.image = loadImage("./assets/player.png")
+    World.add(world,this.bob)
+    }
+    display() {
+        var pos = this.bob.position
+        var angle = this.bob.angle
+
+        push()
+        translate(pos.x, pos.y)
+        rotate(angle)
+        imageMode(CENTER)
+        image(this.image, 0, 0, this.width, this.height)
+        pop()
+    }
+}
