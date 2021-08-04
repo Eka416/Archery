e0c5f6a55dbf747b0a6fd30679805ca27ee9f1ad
Eka416 <87167166+Eka416@users.noreply.github.com>
2021-08-04 14:44:47 -0700

    Add files via upload

--- ComputerArcher.js
+++ ComputerArcher.js
@@ -1,28 +1,30 @@
 class ComputerArcher {
     constructor(x, y, width, height) {
-      var options = {
-        //restitution: 0.8,
-        //friction: 1.0,
-        //density: 1.0,
+      const options = {
         isStatic: true
       };
+  
+      this.body = Matter.Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
-      this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+      //this.collapse = false;
       this.image = loadImage("./assets/computerArcher.png");
+  
       World.add(world, this.body);
-      Matter.Body.setAngle(this.body,PI/2)
-     // this.angle = this.body.angle;
+  
+      Matter.Body.setAngle(this.body, PI / 2); // -90 degree
     }
   
-   display() {
-        var pos = this.body.position
-        var angle = this.body.angle
-        push();
-        translate(pos.x, pos.y);
-        rotate(angle);
-        imageMode(CENTER);
-        image(this.image, 0, 0, this.width, this.height);
-        pop();
+    display() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+       
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
     }
   }
+  
--- PlayerArcher.js
+++ PlayerArcher.js
@@ -1,36 +1,40 @@
 class PlayerArcher {
     constructor(x, y, width, height) {
-      var options = {
-        //restitution: 0.8,
-        //friction: 1.0,
-        //density: 1.0,
+      const options = {
         isStatic: true
       };
+  
+      this.body = Matter.Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
-      this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+      //this.collapse = false;
       this.image = loadImage("./assets/playerArcher.png");
+  
       World.add(world, this.body);
-      Matter.Body.setAngle(this.body,-PI/2)
-      //this.angle = this.body.angle;
+  
+      Matter.Body.setAngle(this.body, -PI / 2); // -90 degree
     }
   
     display() {
-        var pos = this.body.position;
-        var angle = this.body.angle
-        if (keyIsDown(DOWN_ARROW) && angle < -1.27) {
-            angle = angle + 0.02
-            Matter.Body.setAngle(this.body,angle)
-        }
-        if (keyIsDown(UP_ARROW) && angle > -1.77) {
-            angle = angle - 0.02
-            Matter.Body.setAngle(this.body,angle)
-        }
-        push();
-        translate(pos.x, pos.y);
-        rotate(angle);
-        imageMode(CENTER);
-        image(this.image, 0, 0, this.width, this.height);
-        pop();
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      
+      if (keyIsDown(DOWN_ARROW) && angle < -1.2) {
+        angle += 0.01;
+        Matter.Body.setAngle(this.body, angle);
+      }
+  
+      if (keyIsDown(UP_ARROW) && angle > -1.9) {
+        angle -= 0.01;
+        Matter.Body.setAngle(this.body, angle);
+      }
+      
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
     }
   }
+  
