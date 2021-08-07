189b7ae9e55161795512563e1e598c60e8d761da
Eka416 <87167166+Eka416@users.noreply.github.com>
2021-08-07 12:27:30 -0700

    Add files via upload

--- PlayerBase.js
+++ PlayerBase.js
@@ -1,25 +1,26 @@
 class PlayerBase {
-    constructor(x,y,w,h) {
-        var options = {
-            isStatic: true
-        }
-    
-    this.bob = Bodies.rectangle(x,y,w,h,options)
-    this.w = w
-    this.h = h
-    this.image = loadImage("./assets/base1.png")
-    //this.image.scale = 0.1
-    World.add(world,this.bob)
-    }
-    display() {
-        var pos = this.bob.position
-        var angle = this.bob.angle
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
 
-        push()
-        translate(pos.x, pos.y)
-        rotate(angle)
-        imageMode(CENTER)
-        image(this.image, 0, 0, this.width, this.height)
-        pop()
-    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/base1.png");
+
+    World.add(world, this.body);
+  }
+
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
 }
